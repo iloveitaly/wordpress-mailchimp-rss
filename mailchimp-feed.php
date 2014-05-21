99e5c7b0f8eebff5771685f7a5cab512bf1d5a8b
Michael Bianco <iloveitaly@gmail.com>
2014-05-21 12:16:25 -0400

    Inline CSS styles using MC API if available

--- mailchimp-feed.php
+++ mailchimp-feed.php
@@ -13,6 +13,8 @@ Plugin URI: https://github.com/iloveitaly/wordpress-mailchimp-rss
 
 define('MCRSS_IFRAME_REGEX', '#<iframe.+?src="([^"]+)[^>]+></iframe>#i');
 
+define('MCRSS_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
+
 function iloveitaly_format_feed_for_mailchimp($content) {
   // note: depends on an unreleased youtube video plugin
   if(function_exists("iloveitaly_iframe_to_image")) {
@@ -28,6 +30,25 @@ function iloveitaly_format_feed_for_mailchimp($content) {
 
   // link all images to the post
   $content = preg_replace('#<img [^>]*src="([^"]+)"[^>]*>#', "<a target='_blank' href='".get_permalink().'\'>$0</a>', $content);
+
+  // https://github.com/iloveitaly/wordpress-mailchimp-widget
+  if(class_exists('NS_MC_Plugin')) {
+    $mc_api_key = get_option('ns_mc_api_key');
+
+    if(!empty($mc_api_key)) {
+      // MC will NOT inline css on CONTEXT_TEXT coming from an RSS feed
+      // run $content through MC css inliner as a workaround
+
+      $standard_inline_css = "<style type=\"text/css\">" . file_get_contents(MCRSS_PLUGIN_PATH . 'inline-email-styles.css') . "</style>";
+
+      if($css_customization_file = locate_template('inline-email-styles.css')) {
+        $standard_inline_css .= "<style type=\"text/css\">" . file_get_contents($css_customization_file) . "</style>";
+      }
+
+      $mc = new MCAPI($mc_api_key);
+      $content = $mc->inlineCss($standard_inline_css . $content, true);
+    }
+  }
 
   return $content;
 }
--- readme.md
+++ readme.md
@@ -3,7 +3,11 @@ Custom WordPress RSS Feed for MailChimp RSS-to-email
 
 Usage: http://domain.com/blog/?feed=mailchimp
 
-What is does:
+## Customization
+
+In your theme add `inline-email-styles.css`
+
+## How it Works
 
 * Links every image in a post to the post
 * Converts blockquotes to a `p.blockquote`
